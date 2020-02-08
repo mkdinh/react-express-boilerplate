ad3d85c0d5735ce8ed175a99d9aa63cf213b4497
Michael Dinh <mkdinh94@gmail.com>
2020-02-08 09:44:26 -0500

    update controller naming convention

--- routes/auth/signin.auth.js
+++ routes/auth/signin.auth.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const authController = require("../../controllers/auth-controller");
+const authController = require("../../controllers/auth.controller");
 
 // handle different authentication methods
 //--------------------------------------------------------
